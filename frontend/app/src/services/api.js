b18f58f3c122c4c585a6419e59d9a54c788b07cc
Neha@03 <nehatech1679@gmail.com>
2026-05-05 19:07:48 +0530

    api url fixed

--- frontend/app/src/services/api.js
+++ frontend/app/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://assimenttask.onrender.com",
+  baseURL: "https://assimenttask.onrender.com/api/",
 });
 
 API.interceptors.request.use((config) => {
